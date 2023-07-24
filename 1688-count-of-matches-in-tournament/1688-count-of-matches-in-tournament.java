6f322defcb0f1e13a8ba0d6e916a6027e1589583
sanketpatil03 <129180971+sanketpatil03@users.noreply.github.com>
2023-07-24 19:18:05 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (8.25%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int count=0;
+        while(n!=1)
+        {
+            if(n%2==0)
+            {
+                n/=2;
+                count+=n;
+            }
+            else{
+                n=(n-1)/2;
+                count+=n;
+                n++;
+            }
+        }
+        return count;
+    }
+}
