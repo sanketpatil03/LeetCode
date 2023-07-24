246511d4e86925f9ac0676862c0c2b939c6682ce
sanketpatil03 <129180971+sanketpatil03@users.noreply.github.com>
2023-07-24 18:42:58 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (35.22%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int distinctIntegers(int n) {
+        if(n==1)
+        {
+            return n;
+        }
+        else{
+            return n-1;
+        }
+        
+            
+    }
+}
